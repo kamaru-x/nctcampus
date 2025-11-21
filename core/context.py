ae97094fd8b4d61138653dc81ef7ed5c52d8e5d2
Kamarudheen <kamaru.office@gmail.com>
2025-11-21 21:46:21 +0530

    context processor updated

--- core/context.py
+++ core/context.py
@@ -1,10 +1,7 @@
 from dashboard.models import Program, Department
 
-programs = Program.active_objects.all()
-departments = Department.active_objects.all()
-
 def data(request):
     return {
-        'programs_menu': programs,
-        'departments_menu': departments
+        'programs_menu': Program.active_objects.all(),
+        'departments_menu': Department.active_objects.all()
     }
